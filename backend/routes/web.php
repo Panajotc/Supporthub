fe4b6bc3f66522932936037cbeede72cbbb81bb8
Panajotc <panajot7@gmail.com>
2026-06-12 15:39:24 +0300

    refactor(backend): replace default web route response

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'service' => 'supporthub-api',
+        'message' => 'SupportHub API is running.',
+    ]);
 });
